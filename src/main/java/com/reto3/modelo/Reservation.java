7d397e39a1d8c74512c3e664161256bf7b2500dc
CarlinGebyte <carlos.garcia2.mt@correo.usa.edu.co>
2021-10-16 22:11:57 -0500

    Buenas prácticas Reservation.java

--- src/main/java/com/reto3/modelo/Reservation.java
+++ src/main/java/com/reto3/modelo/Reservation.java
@@ -7,86 +7,164 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-@Table(name = "regeservation")
+@Table(name = "reservation")
 public class Reservation implements Serializable {
+    /**
+     * Atributo id de la reservación
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer idReservation;
+    /**
+     * Atributo Fecha inicial de la reservación
+     */
     @Column(name = "startDate")
     //@JsonFormat(pattern="yyyy-MM-dd")
     private Date startDate;
+    /**
+     * Atributo Fecha de devolución
+     */
     @Column(name = "devolutionDate")
     //@JsonFormat(pattern="yyyy-MM-dd")
     private Date devolutionDate;
-
+    /**
+     * Atributo estado de la reservación
+     */
     @Column(name = "status")
     private String status = "created";
 
+    /**
+     * Relación muchos a uno tabla car
+     */
     @ManyToOne
     @JoinColumn(name = "idReservation")
     @JsonIgnoreProperties("reservations")
     private Car car;
+    /**
+     * Relación muchos a uno tabla client
+     */
     @ManyToOne
     @JoinColumn(name = "idClient")
     @JsonIgnoreProperties({"reservations", "messages"})
     private Client client;
 
+    /**
+     * Atributo score
+     */
     @Column(name = "score")
     private Integer score;
 
+    // Getter y Setter
+
+    /**
+     * Getter idReservation
+     * @return
+     */
     public Integer getIdReservation() {
         return idReservation;
     }
 
+    /**
+     * Setter idReservation
+     * @param idReservation
+     */
     public void setIdReservation(Integer idReservation) {
         this.idReservation = idReservation;
     }
 
+    /**
+     * Getter startDate
+     * @return
+     */
     public Date getStartDate() {
         return startDate;
     }
 
+    /**
+     * Setter startDate
+     * @param startDate
+     */
     public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
 
+    /**
+     * Getter devolutionDate
+     * @return
+     */
     public Date getDevolutionDate() {
         return devolutionDate;
     }
 
+    /**
+     * Setter devolutionDate
+     * @param devolutionDate
+     */
     public void setDevolutionDate(Date devolutionDate) {
         this.devolutionDate = devolutionDate;
     }
 
+    /**
+     * Getter status
+     * @return
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Setter status
+     * @param status
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * Getter car
+     * @return
+     */
     public Car getCar() {
         return car;
     }
 
+    /**
+     * Setter car
+     * @param car
+     */
     public void setCar(Car car) {
         this.car = car;
     }
 
+    /**
+     * Getter client
+     * @return
+     */
     public Client getClient() {
         return client;
     }
 
+    /**
+     * Setter client
+     * @param client
+     */
     public void setClient(Client client) {
         this.client = client;
     }
 
+    /**
+     * Getter score
+     * @return
+     */
     public Integer getScore() {
         return score;
     }
 
+    /**
+     * Setter score
+     * @param score
+     */
     public void setScore(Integer score) {
         this.score = score;
     }
